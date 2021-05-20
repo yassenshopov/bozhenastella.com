3a30974e871792700a931abc00d8d0a5e6fa197d
Yassen Shopov <83444316+yassenshopov@users.noreply.github.com>
2021-05-20 10:56:10 +0300

    Update Blog_JS.js

--- blog/Blog_JS.js
+++ blog/Blog_JS.js
@@ -1,9 +1,3 @@
-/* Open when someone clicks on the span element */
-function openNav() {
-  document.getElementById("myNav").style.width = "100%";
-}
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
+function animate_menu(x) {
+    x.classList.toggle("change");
 }
