ac8ccb4152dbd12a88cd9d3882cc37ee66b965aa
Yassen Shopov <yassenshopov00@gmail.com>
2022-10-27 12:44:26 +0300

    Created React version of the website

---
+++ react-version/src/components/Footer.js
@@ -0,0 +1,12 @@
+// import '../App.css';
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="Footer">
+      <p>Copyright © 2022 - Designed and maintained by <a href="https://github.com/yassenshopov">Yassen Shopov</a></p>
+    </footer>
+  );
+}
+
+export default Footer;
